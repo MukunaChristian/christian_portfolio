0dc788db66e796710d782a8a4d17ea38b8a63116
MukunaChristian <christianmbuyimukuna@gmail.com>
2024-11-14 08:29:27 +0200

    added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/christian-portfolio/', // Add your repository name here
+  base: '/christian_portfolio/', // Add your repository name here
 })

0a8083ea0f3dc583d9b42607efccdb82d6a7bcf8
MukunaChristian <christianmbuyimukuna@gmail.com>
2025-08-08 23:49:40 +0200

    Updates

--- src/data/roles.js
+++
@@ -1,6 +0,0 @@
-export const roles = [
-    "Software Developer",
-    "ICT Graduate",
-    "Tech Enthusiast",
-    "Web Developer"
-];
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/christian_portfolio/', // Add your repository name here
-})
